2aaa7489cbf9217436b666cfa3af7ab4f2059202
gregor <gregor.barth@fu-berlin.de>
2018-09-24 13:57:29 +0200

    initial commit

---
+++ SrcBehaviorLoader/PluginLoader.cpp
@@ -0,0 +1,55 @@
+#include "PluginLoader.h"
+
+#include <QDebug>
+
+PluginLoader::PluginLoader(QObject *parent)
+{
+	m_PluginLoader = new QPluginLoader(this);
+}
+
+bool PluginLoader::loadPluginFromFilename(QString const& filename)
+{
+	bool retval = false;
+	if (m_PluginLoader->isLoaded()) {
+
+		m_PluginLoader->unload();
+	}
+
+	bool isLib = QLibrary::isLibrary(filename);
+
+	if (isLib) {
+
+		m_PluginLoader->setFileName(filename);
+
+		readMetaDataFromPlugin();
+
+		retval = m_PluginLoader->load();
+		QString s = m_PluginLoader->errorString();
+		std::string ss = s.toStdString();
+
+        if (!m_PluginLoader->isLoaded())
+		{
+		    qWarning() << ss.c_str();
+		}
+	}
+	else {
+		retval = false;
+	}
+
+	return retval;
+}
+
+IBehaviourFactory* PluginLoader::getPluginInstance()
+{
+	return qobject_cast<IBehaviourFactory*>(m_PluginLoader->instance());
+}
+
+QJsonObject PluginLoader::getPluginMetaData() const
+{
+	return m_MetaData;
+}
+
+void PluginLoader::readMetaDataFromPlugin()
+{
+	m_MetaData = m_PluginLoader->metaData().value("MetaData").toObject();
+}
---
+++ SrcBehaviorLoader/PluginLoader.h
@@ -0,0 +1,27 @@
+
+#ifndef PLUGINLOADER_H
+#define PLUGINLOADER_H
+
+#include "QPluginLoader"
+#include "QStringListModel"
+#include "IBehaviourFactory.h"
+#include "QPointer"
+
+class PluginLoader : QObject
+{ 
+	Q_OBJECT
+public:
+	explicit PluginLoader(QObject *parent = 0);
+
+	bool loadPluginFromFilename(QString const& filename);
+	IBehaviourFactory *getPluginInstance();
+	QJsonObject getPluginMetaData() const;
+private:
+
+	void readMetaDataFromPlugin();
+
+	QPluginLoader *m_PluginLoader;
+	QJsonObject m_MetaData;
+};
+
+#endif // PLUGINLOADER_H
